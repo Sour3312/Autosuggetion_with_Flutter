274f6bcd752d5c644c373cd760772f54a03b15b3
Sourav <sourabh.kumar3312@gmail.com>
2022-06-23 17:16:19 +0530

    demo added

---
+++ demo/lib/navbar.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+
+class NavBar extends StatefulWidget {
+  NavBar({Key? key}) : super(key: key);
+
+  @override
+  State<NavBar> createState() => _NavBarState();
+}
+
+class _NavBarState extends State<NavBar> {
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      child: Scaffold(
+        bottomNavigationBar: Container(
+          height: 60,
+          decoration: BoxDecoration(
+            color: Theme.of(context).primaryColor,
+            borderRadius: const BorderRadius.only(
+              topLeft: Radius.circular(20),
+              topRight: Radius.circular(20),
+            ),
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              IconButton(
+                enableFeedback: false,
+                onPressed: () {},
+                icon: const Icon(
+                  Icons.home_outlined,
+                  color: Colors.white,
+                  size: 35,
+                ),
+              ),
+              IconButton(
+                enableFeedback: false,
+                onPressed: () {},
+                icon: const Icon(
+                  Icons.work_outline_outlined,
+                  color: Colors.white,
+                  size: 35,
+                ),
+              ),
+              IconButton(
+                enableFeedback: false,
+                onPressed: () {},
+                icon: const Icon(
+                  Icons.widgets_outlined,
+                  color: Colors.white,
+                  size: 35,
+                ),
+              ),
+              IconButton(
+                enableFeedback: false,
+                onPressed: () {},
+                icon: const Icon(
+                  Icons.person_outline,
+                  color: Colors.white,
+                  size: 35,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
